28cbaed10695b9266e53c61de94567d87b679c86
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-10-18 19:43:04 -0300

    feat: addding router on main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router";
 
-createRoot(document.getElementById('root')).render(
-  <Home />
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>
+);
