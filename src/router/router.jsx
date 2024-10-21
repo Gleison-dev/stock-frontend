8d92c9050da4bc1499f0ffe7b27ca040a76764ca
Gleison Ribeiro Gomes <gleisonrgomes@gmail.com>
2024-10-21 16:54:44 -0300

    feat: creating routes from all pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/index";
+import RegisterProduct from "../pages/RegisterProduct/index";
+import GetProducts from "../pages/GetProducts/index";
+import DeleteProduct from "../pages/DeleteProduct/index";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />
-  }
+    element: <Home />,
+  },
+  {
+    path: "/registerProduct",
+    element: <RegisterProduct />,
+  },
+  {
+    path: "/getAllProducts",
+    element: <GetProducts />,
+  },
+  {
+    path: "/deleteProduct",
+    element: <DeleteProduct />,
+  },
 ]);
 
 export default router;
